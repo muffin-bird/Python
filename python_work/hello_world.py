a4045f4714486e8eb982fb235eb018671339cb1a
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-19 14:55:50 +0900

    変数のNameErrorを避ける

--- python_work/hello_world.py
+++ python_work/hello_world.py
@@ -1,7 +1,11 @@
 print("Hello Python world!")
 
-#変数
+# 変数
 message = "Hello Python world!"
 print(message)
 message = "Hello 最短距離でゼロからしっかり学ぶ　python入門 world!"
 print(message)
+
+# NameError
+mesage = "こんにちは最短距離でゼロからしっかり学ぶPython入門の読者のみなさん!"
+print(mesage)
