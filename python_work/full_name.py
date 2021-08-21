0950357bd38ea9a0c2436aa1e6778dbe732867d0
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-21 22:51:49 +0900

    文字列の中で変数を使用する
    close #10

---
+++ python_work/full_name.py
@@ -0,0 +1,7 @@
+# 文字列の中の変数
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"こんにちは{full_name.title()}!")
+message = f"こんにちは{full_name.title()}!"
+print(message)
