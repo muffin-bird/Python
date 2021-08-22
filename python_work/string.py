3e4331d7aee14e537961ac9a8deda9af8c3738ab
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-22 21:48:59 +0900

    文字列にタブや改行を加える
    close #11

--- python_work/string.py
+++ python_work/string.py
@@ -2,3 +2,9 @@
 print("私は友達に'好きな言語はPythonです!'と言った。")
 print('プログラミング言語"Python"の名前はヘビではなくモンティ・パイソンから来ています。')
 print("Python's strengths (Pythonの強み)は多様で協力的なコミュニティーです。")
+
+# 空白文字
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
