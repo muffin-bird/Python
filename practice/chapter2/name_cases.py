16b0626bea07d6c02d924ab7ca027ecaa4eeee0c
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-25 20:11:07 +0900

    演習問題 2-6
    close #17

--- practice/chapter2/name_cases.py
+++ practice/chapter2/name_cases.py
@@ -9,6 +9,9 @@ print(full_name.upper())
 print(full_name.lower())
 
 # 演習問題 2-5
-celebrity = "スティーブ・ジョブズ"
-witticism = f'{celebrity}は"僕は、宇宙をへこませたい。"と言った'
-print(witticism)
+print('スティーブ・ジョブズは"僕は、宇宙をへこませたい。"と言った。')
+
+# 演習問題 2-6
+famous_person = "スティーブ・ジョブズ"
+message = f'{famous_person}は"僕は、宇宙をへこませたい。"と言った。'
+print(message)
