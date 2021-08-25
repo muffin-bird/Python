a1544090647df88c0b972571036e9d9d447c4d7a
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-08-25 19:51:29 +0900

    演習問題 2-4
    close #15

---
+++ practice/chapter2/name_cases.py
@@ -0,0 +1,9 @@
+# 演習問題 2-3
+name = "Muffin"
+print(f"こんにちは{name},今日はPythonを学びますか？")
+
+# 演習問題 2-4
+full_name = "muffin man"
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
--- practice/name_cases.py
+++
@@ -1,3 +0,0 @@
-# 演習問題 2-3
-name = "Muffin"
-print(f"こんにちは{name},今日はPythonを学びますか？")
