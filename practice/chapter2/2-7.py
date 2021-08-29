4c9060fdbe1e9ed00c518448a09780955a94256f
LeonTeraoka <brt765g@gmail.com>
2021-08-29 23:24:40 +0900

    演習問題 2-7 参考
    issueが残っていたので、参考にコードを書きました。

---
+++ practice/chapter2/2-7.py
@@ -0,0 +1,9 @@
+name = " muffin "
+
+print(f"\t---自己紹介---")
+print(f"私の名前は{name.title()}です。")
+print(f"lstripを使うと{name.title().lstrip()}のように左に寄ります。")
+print(f"rstripを使うと{name.title().rstrip()}のように左に寄ります。")
+print(f"stripを使うと{name.title().strip()}のようにスッキリします。")
+print(f"フルネーム：\n\tMuffin\n\tFix\n\tBird")
+print(f"\t---以上---")
