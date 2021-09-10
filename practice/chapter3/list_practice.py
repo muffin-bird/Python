ed0bd9d1ea127eb94971ebfd9959693fc6004760
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-10 12:27:38 +0900

    演習問題 3-3
    close #41

--- practice/chapter3/list_practice.py
+++ practice/chapter3/list_practice.py
@@ -6,3 +6,7 @@ print(names[-2])
 print(f"こんにちは{names[0]}!")
 print(f"こんにちは{names[1]}!")
 print(f"こんにちは{names[2]}!")
+
+# 演習問題 3-3
+carbrands = ["Honda", "Toyota", "suzuki"]
+print(f"私は{carbrands[1]}の車がほしい。")
