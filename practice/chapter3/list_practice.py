94790bfba82ec53df0898ee669d6b2d43035a81e
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-15 17:35:22 +0900

    演習問題 3-4
    close #46

--- practice/chapter3/list_practice.py
+++ practice/chapter3/list_practice.py
@@ -10,3 +10,13 @@ print(f"こんにちは{names[2]}!")
 # 演習問題 3-3
 carbrands = ["Honda", "Toyota", "suzuki"]
 print(f"私は{carbrands[1]}の車がほしい。")
+
+# 演習問題 3-4
+persons = ["muffin", "milk", "police"]
+
+name = persons[0]
+print(f"こんにちは{name}さん、夕食はいかがでしょうか。")
+name_2 = persons[1]
+print(f"こんにちは{name_2}さん、夕食はいかがでしょうか。")
+name_3 = persons[2]
+print(f"こんにちは{name_3}さん、夕食はいかがでしょうか。")
