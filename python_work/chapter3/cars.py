3d8ed24c20e4ff99c6910280154828589375e824
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-16 19:03:59 +0900

    sort()メソッドでリストを永続的にソートする
    close #51

---
+++ python_work/chapter3/cars.py
@@ -0,0 +1,14 @@
+# リストをアルファベット順に変更
+cars = ["bmw", "audi", "toyota", "subaru"]
+cars.sort()
+print(cars)
+
+# リストをアルファベット逆順に変更
+cars.sort(reverse=True)
+print(cars)
+
+# 文字列が日本語
+cars = ["bmw", "アウディ", "日産", "すばる"]
+cars.sort()
+print(cars)
+
