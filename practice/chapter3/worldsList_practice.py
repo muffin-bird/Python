3da32fcb9ddcd52c76a528c19248007b738a2cee
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-17 12:20:28 +0900

    演習問題 3-8
    close #55

---
+++ practice/chapter3/worldsList_practice.py
@@ -0,0 +1,13 @@
+# 演習問題 3-8
+worlds = ["America", "Switzerland", "Japan", "Canada", "Brazil"]
+print(worlds)
+print(sorted(worlds))
+print(worlds)
+worlds.reverse()
+print(worlds)
+worlds.reverse()
+print(worlds)
+worlds.sort()
+print(worlds)
+worlds.sort(reverse=True)
+print(worlds)
