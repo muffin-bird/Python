51ade0440601ac4ce35de977b470d8bdb33c7f7b
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-23 00:34:38 +0900

    forループの後に何かを実行する
    close #62

--- python_work/chapter4/magicians.py
+++ python_work/chapter4/magicians.py
@@ -1,3 +1,8 @@
+# forループ後の処理
 magicians = ["alice", "david", "carolina"]
 for magician in magicians:
-	print(magician)
+	print(f"{magician.title()}は素晴らしい手品を演じた!")
+	print(f"私は{magician.title()}の次の手品が待ちきれない。\n")
+
+# インデントされていない
+print("みなさんありがとう。素晴らしい手品ショーでした!")
