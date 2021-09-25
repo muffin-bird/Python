a25b9126864c9012538029205c70b1236103b1b0
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-25 14:39:25 +0900

    演習問題 4-2
    close #70

---
+++ practice/chapter4/for_practice.py
@@ -0,0 +1,13 @@
+# 演習問題 4-1
+pizzas = ["tomato", "cheese", "honey"]
+for pizza in pizzas:
+	print(f"私は{pizza.title()}pizzaが好きです。\n")
+
+print("私はpizzaが大好きです。\n")
+
+# 演習問題 4-2
+animals = ["dog", "cat", "mouse"]
+for animal in animals:
+	print(f"{animal}は素晴らしいpetです。\n")
+
+print("このanimalsたちは素晴らしいpetです。")
