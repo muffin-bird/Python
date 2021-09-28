5e9ef8d8b97c58481a54841c17752f7457e0cebc
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-28 11:29:48 +0900

    演習問題 4-5
    close #78

--- practice/chapter4/value_practice.py
+++ practice/chapter4/value_practice.py
@@ -5,3 +5,9 @@ for value in range(1, 21):
 # 演習問題 4-4
 for value in range(1, 1_000_001):
 	print(value)
+
+# 演習問題 4-5
+numbers = list(range(1, 1_000_001))
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
