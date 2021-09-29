2bc9d500eb0f69aab3addef19a8e8e28b5e36d81
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-29 16:37:16 +0900

    スライスによるループ
    close #84

--- python_work/chapter4/players.py
+++ python_work/chapter4/players.py
@@ -5,3 +5,10 @@ print(players[1:4])
 print(players[:4])
 print(players[2:])
 print(players[-3:])
+
+# スライスによるループ
+players = ["charles", "martina", "michael", "florence", "eli"]
+
+print("チームの最初の3人です。")
+for player in players[:3]:
+	print(player.title())
