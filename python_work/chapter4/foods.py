c09817646b84e80788928009d9393370d9107dab
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-09-30 09:54:20 +0900

    リストをコピーする
    close #85

---
+++ python_work/chapter4/foods.py
@@ -0,0 +1,13 @@
+# リストをコピーする
+my_foods = ["ピザ", "だんご", "ケーキ"]
+friend_foods = my_foods[:]
+
+# 格リストに要素を追加
+my_foods.append("チョコレート")
+friend_foods.append("アイスクリーム")
+
+print("私が好きな食べ物")
+print(my_foods)
+
+print("\n友達が好きな食べ物")
+print(friend_foods)
