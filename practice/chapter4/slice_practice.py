1f8c096287cfd2182aa0dc2bc0f1543bcfdb6b74
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-01 21:07:48 +0900

    演習問題 4-11
    close #87

--- practice/chapter4/slice_practice.py
+++ practice/chapter4/slice_practice.py
@@ -11,3 +11,18 @@ for number in numbers[3:6]:
 print("\nリストの最後の3つの要素です。")
 for number in numbers[-3:]:
 	print(number)
+
+# 演習問題 4-11
+pizzas = ["tomato", "cheese", "honey"]
+friend_pizzas = pizzas[:]
+
+pizzas.append("chili")
+friend_pizzas.append("seafood")
+
+print("\n私の好きなpizza")
+for pizza in pizzas:
+	print(pizza)
+
+print("\n友達が好きなpizza")
+for friend_pizza in friend_pizzas:
+	print(friend_pizza)
