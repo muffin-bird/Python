7fcb59fd03ee6cdba8c9140d6eaf633a3c008f0a
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-02 11:11:23 +0900

    タプルを上書きする
    close #91

--- python_work/chapter4/dimrnsions.py
+++ python_work/chapter4/dimrnsions.py
@@ -6,3 +6,14 @@ print(dimensions[1])
 # ループ
 for dimension in dimensions:
 	print(dimension)
+
+# タプル上書き
+dimensions = (200, 50)
+print("元のサイズ")
+for dimension in dimensions:
+	print(dimension)
+
+dimensions = (400, 100)
+print("\n変更したサイズ")
+for dimension in dimensions:
+	print(dimension)
