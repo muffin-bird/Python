ca106d1ee77753ab0661b8a32821f640f836da81
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-03 22:00:11 +0900

    演習問題 4-13
    close #92

---
+++ practice/chapter4/buffet_practice.py
@@ -0,0 +1,12 @@
+# 演習問題 4-13
+foods = ("potato", "cheese", "ham", "corn", "bread")
+
+for food in foods:
+	print(food)
+
+print("\nメニュー変更")
+
+foods = ("potato", "cheese", "ham", "cherry", "onion")
+
+for food in foods:
+	print(food)
