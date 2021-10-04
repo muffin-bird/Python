677ba7939a1e8c9200774439267191cd9698c1f2
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-04 18:26:19 +0900

    演習問題 4-15
    close #100

---
+++ practice/chapter4/code_review.py
@@ -0,0 +1,18 @@
+# 演習問題　4-15
+foods = ("potato", "cheese",
+		 "ham", "corn", "bread")
+print(foods)
+
+my_foods = [
+	"ピザ", 
+	"だんご", 
+	"ケーキ",
+	]
+print(my_foods)
+
+pizzas = [
+	"tomato", 
+	"cheese", 
+	"honey",
+]
+print(pizzas)
