e6bebca471b784a98a4e71d8d39c52d38f10b08e
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-07 00:08:30 +0900

    数値の比較
    close #105

---
+++ python_work/chapter5/magic_number.py
@@ -0,0 +1,13 @@
+# 数値の比較
+answer = 17
+
+if answer != 42:
+	print("正しい答えではありません。もう一度挑戦してください!")
+
+# 様々な比較演算子
+age = 19
+print(age < 21)
+print(age < 21)
+print(age <= 21)
+print(age > 21)
+print(age >= 21)
