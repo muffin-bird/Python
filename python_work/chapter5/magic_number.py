c72d995b040badbdfd41ef0271dfc64f8c2825bb
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-08 18:49:17 +0900

    複数の条件を確認する
    close #106

--- python_work/chapter5/magic_number.py
+++ python_work/chapter5/magic_number.py
@@ -11,3 +11,17 @@ print(age < 21)
 print(age <= 21)
 print(age > 21)
 print(age >= 21)
+
+# and
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+age_1 = 22
+print(age_0 >= 21 and age_1 >= 21)
+
+# or
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21)
+age_0 = 18
+print(age_0 >= 21 or age_1 >= 21)
