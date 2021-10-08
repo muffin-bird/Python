63b69d1921d69c7d8d4d00c71c099907c223c93f
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-09 01:54:38 +0900

    値がリストに存在することを確認する
    close #107

--- python_work/chapter5/toppings.py
+++ python_work/chapter5/toppings.py
@@ -3,3 +3,9 @@ requested_topping = "マッシュルーム"
 
 if requested_topping != "アンチョビ":
 	print("アンチョビを注文してください!")
+
+# inキーワード
+requested_topping = ["マッシュルーム", "オニオン", "パイナップル"]
+
+print("マッシュルーム" in requested_topping)
+print("ペパロニ" in requested_topping)
