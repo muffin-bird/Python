6fa353f7b7d879b1c8e56343d8cee2a5fa449140
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-12 20:21:29 +0900

    演習問題 5-1
    close #110

---
+++ practice/chapter5/conditions_practice.py
@@ -0,0 +1,38 @@
+# 演習問題 5-1
+car = "subaru"
+print("car == 'subaru'の結果をTrueと予測します。")
+print(car == "subaru")
+
+print("\ncar == 'audi'の結果をFalseと予測します。")
+print(car == "audi")
+
+print("\n10個以上の条件テスト")
+fruit = "apple"
+print(fruit == "apple")
+
+fruit = "remon"
+print(fruit == "remon")
+
+fruit = "orenge"
+print(fruit == "orenge")
+
+age = 21
+print(age >= 20 and age <= 50 )
+
+age = 35
+print(age >= 20 or age <= 20 )
+
+fruit = "apple"
+print(fruit == "remon")
+
+fruit = "remon"
+print(fruit == "apple")
+
+fruit = "orenge"
+print(fruit == "peach")
+
+age = 10
+print(age >= 20 and age <= 50 )
+
+age = 35
+print(age >= 40 or age <= 20 )
