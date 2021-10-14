6eefad32e391c318606f0aa59f749826a57e0ac5
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-14 18:57:41 +0900

    演習問題 5-2
    close #111

---
+++ practice/chapter5/conditional_tests.py
@@ -0,0 +1,22 @@
+# 演習問題 5-2
+car = "audi"
+print(car == "audi")
+print(car == "Audi")
+
+car = "TOYOTA".lower()
+
+print(car == "TOYOTA")
+
+number = 20
+print(number < 25)
+print(number <= 20)
+print(number > 10)
+print(number >= 20)
+
+age = 41
+print(age >= 20 and age <= 50)
+print(age >= 40 or age <= 20)
+
+car = ["Toyota", "Suzuki", "Audi"]
+print("Toyota" in car)
+print("toyota"  not in car)
