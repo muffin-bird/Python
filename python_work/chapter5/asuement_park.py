7a09c01f8df5202dd7d15f70a95e1e36d665fbce
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-17 00:17:48 +0900

    複数のelifブロックを使用する
    close #115

--- python_work/chapter5/asuement_park.py
+++ python_work/chapter5/asuement_park.py
@@ -1,5 +1,6 @@
 # if-elif-else文
 age = 12
+
 if age < 4:
 	print("入場料金は0円です。")
 elif age < 18:
@@ -15,6 +16,20 @@ elif age < 18:
 	price = 2500
 else:
 	price = 4000
+
+print(f"入場料金は{price}円です。")
+
+# 複数のelif
+age =12
+
+if age < 4:
+	price = 0
+elif age < 18:
+	price = 2500
+elif age < 65:
+	price = 4000
+else:
+	price = 2000
 
 print(f"入場料金は{price}円です。")
 	
