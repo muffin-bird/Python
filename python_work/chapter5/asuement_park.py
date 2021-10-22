83ccec8045412372f244d2a80df0b24226622482
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-22 12:46:03 +0900

    elseブロックを省略する
    close #116

--- python_work/chapter5/asuement_park.py
+++ python_work/chapter5/asuement_park.py
@@ -32,4 +32,18 @@ else:
 	price = 2000
 
 print(f"入場料金は{price}円です。")
+
+# elseの省略
+age = 12
+
+if age < 4:
+	price = 0
+elif age < 18:
+	price = 2500
+elif age < 65:
+	price = 4000
+elif age >= 65:
+	price = 2000
+
+print(f"入場料金は{price}円です。")
 	
