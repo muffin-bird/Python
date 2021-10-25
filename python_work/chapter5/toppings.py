e1335c8a54b91c0a6dc5c1d9f5e1918f12d76706
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-26 00:41:38 +0900

    複数の条件をテストする
    close #117

--- python_work/chapter5/toppings.py
+++ python_work/chapter5/toppings.py
@@ -9,3 +9,15 @@ requested_topping = ["マッシュルーム", "オニオン", "パイナップ�
 
 print("マッシュルーム" in requested_topping)
 print("ペパロニ" in requested_topping)
+
+# 複数の条件をテストする
+requested_toppings = ["マッシュルーム", "エクストラチーズ"]
+
+if "マッシュルーム" in requested_toppings:
+	print("マッシュルームを追加する。")
+if "ペパロニ" in requested_toppings:
+	print("ペパロニを追加する。")
+if "エクストラチーズ" in requested_toppings:
+	print("エクストラチーズを追加する。")
+
+print("\nピザができました!")
