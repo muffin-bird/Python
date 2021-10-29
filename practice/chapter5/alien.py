36215e9fe3b356c978a3243804f2161bcfb0f825
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-10-30 05:14:26 +0900

    演習問題 5-4
    close #119

--- practice/chapter5/alien.py
+++ practice/chapter5/alien.py
@@ -8,3 +8,11 @@ alien_color = "緑"
 
 if alien_color == "緑":
 	print("プレイヤーが5点を獲得した")
+
+# 演習問題 5-4
+alien_color = "赤"
+
+if alien_color == "緑":
+	print("プレイヤーが5点を獲得した")
+else:
+	print("プレイヤーが10点を獲得した")
