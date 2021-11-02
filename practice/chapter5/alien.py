8f895188fe20bd4d1f37b602f4180902885707b3
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-11-02 12:32:58 +0900

    演習問題 5-6
    close #121

--- practice/chapter5/alien.py
+++ practice/chapter5/alien.py
@@ -44,3 +44,19 @@ elif alien_color == "黄":
 	print("プレイヤーが10点を獲得した")
 else:
 	print("プレイヤーが15点を獲得した")
+
+# 演習問題 5-6
+age = 12
+
+if age < 2:
+	print("赤ちゃん")
+elif age < 4:
+	print("幼児")
+elif age < 13:
+	print("子供")
+elif age < 20:
+	print("ティーンエイジャー")
+elif age < 65:
+	print("大人")
+else:
+	print("高齢者")
