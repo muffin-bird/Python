3f1ce189085a32e52bd2b227850379b0f01dd368
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-11-03 14:00:40 +0900

    演習問題 5-7
    close #122

--- practice/chapter5/alien.py
+++ practice/chapter5/alien.py
@@ -60,3 +60,17 @@ elif age < 65:
 	print("大人")
 else:
 	print("高齢者")
+
+# 演習問題 5-7
+favorite_fruits = ["リンゴ", "オレンジ", "レモン"]
+
+if "ピーチ" in favorite_fruits:
+	print("あなたは本当にピーチが好きですね！")
+elif "レモン" in favorite_fruits:
+	print("あなたは本当にレモンが好きですね！")
+elif "メロン" in favorite_fruits:
+	print("あなたは本当にメロンが好きですね！")
+elif "ザクロ" in favorite_fruits:
+	print("あなたは本当にザクロ好きですね！")
+elif "マスカット" in favorite_fruits:
+	print("あなたは本当にマスカットが好きですね！")
