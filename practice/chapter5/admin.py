0a41b54cbfa17d7ae790243d9dbdeb1d1f4af53f
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-11-19 00:02:20 +0900

    演習問題 5-10
    close #128

--- practice/chapter5/admin.py
+++ practice/chapter5/admin.py
@@ -19,3 +19,16 @@ if users:
             print(f"こんにちは{user}、またログインしてくれてありがとう。")
 else:
     print("ユーザー募集中です！")
+
+# 演習問題 5-10
+current_users = ["muffin", "milk", "rocket", "police", "island"]
+
+new_users = ["Muffin", "Milk", "Remon", "MyMan", "Batter"]
+
+new_users_lower = [new_user_lower.lower() for new_user_lower in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in new_users_lower:
+        print(f"{new_user}は利用できないため、別のユーザー名を入力してください！")
+    else:
+        print(f"{new_user}、ユーザー名は利用可能です！")
