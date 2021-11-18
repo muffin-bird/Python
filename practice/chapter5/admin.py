99dab0ed24cad9c8d0bd1d392097c6cc257a0cde
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-11-18 18:27:37 +0900

    演習問題 5-9
    close #127

--- practice/chapter5/admin.py
+++ practice/chapter5/admin.py
@@ -6,3 +6,16 @@ for user in users:
 		print("こんにちはadmin、状況のレポートを見ますか？")
 	else:
 		print(f"こんにちは{user}、またログインしてくれてありがとう。")
+
+# 演習問題 5-9
+
+users = []
+
+if users:
+    for use in users:
+        if user == 'admin':
+            print("こんにちはadmin、状況のレポートを見ますか？")
+        else:
+            print(f"こんにちは{user}、またログインしてくれてありがとう。")
+else:
+    print("ユーザー募集中です！")
