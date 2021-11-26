b97cb32db70d822f1208bd1c12555e13a0303b7e
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-11-27 00:26:42 +0900

    辞書の値にアクセスする
    close #136

--- python_work/chapter6/alien.py
+++ python_work/chapter6/alien.py
@@ -3,3 +3,10 @@ alien_0 = {"color": "green", "points": 5}
 
 print(alien_0["color"])
 print(alien_0["points"])
+
+# 辞書の値にアクセスする
+alien_0 = {"color": "green", "points": 5}
+print(alien_0["color"])
+
+new_points = alien_0["points"]
+print(f"{new_points}点獲得しました!")
