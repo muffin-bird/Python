a024e6469963e0623920f21fc6fd1c70d8836c14
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-12-01 16:41:22 +0900

    新しいキーと値のペアを追加する
    close #137

--- python_work/chapter6/alien.py
+++ python_work/chapter6/alien.py
@@ -10,3 +10,11 @@ print(alien_0["color"])
 
 new_points = alien_0["points"]
 print(f"{new_points}点獲得しました!")
+
+# 新しいキーと値のペアを追加する
+alien_0 = {"color": "green", "points": 5}
+print(alien_0["color"])
+
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
