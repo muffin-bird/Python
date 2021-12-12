176040f9d66b19cc98ef51dde71e3965ef8a9511
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-12-12 11:56:32 +0900

    似たようなオブジェクトを格納した辞書
    close #141

---
+++ python_work/chapter6/favorite_languages.py
@@ -0,0 +1,10 @@
+# 似たようなオブジェクトを格納した辞書
+favorite_languages = {
+	'jen': 'python',
+	'sarah': 'c',
+	'edward': 'ruby',
+	'phil': 'python',
+}
+
+language = favorite_languages['sarah'].title()
+print(f"Sarahの好きなプログラミング言語は{language}です。")
