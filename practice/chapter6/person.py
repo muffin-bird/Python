3042ce0a8a6e8639568fcfde1b8c7121adc4edd1
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-12-15 12:45:08 +0900

    演習問題 6-1
    close #143

---
+++ practice/chapter6/person.py
@@ -0,0 +1,11 @@
+person = {
+	'first_name': 'muffin',
+	'last_name': 'man',
+	'age': 47,
+	'city': 'osaka'
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
