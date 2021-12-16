52dc611a52b70e072354c3d3673b1c47eaff5531
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2021-12-16 11:19:41 +0900

    演習問題 6-2
    close #144

--- practice/chapter6/person.py
+++ practice/chapter6/person.py
@@ -1,3 +1,4 @@
+# 演習問題 6-1
 person = {
 	'first_name': 'muffin',
 	'last_name': 'man',
@@ -9,3 +10,27 @@ print(person['first_name'])
 print(person['last_name'])
 print(person['age'])
 print(person['city'])
+
+# 演習問題 6-2
+favorite_numbers = {
+	'muffin': 10,
+	'milk': 20,
+	'police': 30,
+	'island': 40,
+	'myman': 50
+}
+
+number= favorite_numbers['muffin']
+print(f"muffinの好きな数字は{number}です。")
+
+number= favorite_numbers['milk']
+print(f"milkの好きな数字は{number}です。")
+
+number= favorite_numbers['police']
+print(f"policeの好きな数字は{number}です。")
+
+number= favorite_numbers['island']
+print(f"islandの好きな数字は{number}です。")
+
+number= favorite_numbers['myman']
+print(f"mymanbの好きな数字は{number}です。")
